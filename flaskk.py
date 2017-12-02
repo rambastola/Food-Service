fc1043bc42e980b37e73c033c91353102085ff1a
Ram Bastola <rambastola7@gmail.com>
2017-12-02 16:53:20 -0500

    Add files via upload

---
+++ flaskk.py
@@ -0,0 +1,37 @@
+
+import requests, json
+
+class Food:
+
+    def __init__(self):
+        self.user2 = int(input("What's your calories take for the day?"))
+        self.remaining_colories=0
+
+    def cal_count(self,us):
+        self.user=us
+        get = requests.get("https://api.edamam.com/search?q=" + us,"&app_id=34e43581&app_key=%20ab719d319c75a45c081aeebdf8d24ec1")
+        data = get.content
+        pull = json.loads(data)
+        items = pull["q"]
+        self.food_calories = pull["count"]
+        print(items, self.food_calories)
+        return self.food_calories  # calories of the food
+
+    def user_cal(self, foodcalories):
+
+        remaining_calories = self.user2 - foodcalories
+        print(remaining_calories)
+        while remaining_calories > 0:
+            second_food = input("another food")
+            b = self.cal_count(second_food)
+            remaining_calories = remaining_calories - b
+            print("Your remaining calories is", remaining_calories)
+
+def main():
+
+    f = Food()
+    first_food = input("food ")
+    a = f.cal_count(first_food)
+    f.user_cal(a)
+
+main()
